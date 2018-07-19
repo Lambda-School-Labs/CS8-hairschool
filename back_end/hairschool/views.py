460a09bf4f9bd4e9dd4b5cc1eab5199822aa61ca
nourmhassan <nour-pop1@hotmail.com>
2018-07-19 08:58:03 -0700

    got sign up working

--- back_end/hairschool/views.py
+++ back_end/hairschool/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import SignUp
+from .serializers import SignUpSerializer
+
+
+class SignUpView(viewsets.ModelViewSet):
+    queryset = SignUp.objects.all()
+    serializer_class = SignUpSerializer
