30e9fe2ea9c69cbc68e9585ab5b29bebd50e24a6
Omoniyi Alimi <omoniyialimi@Omoniyis-MacBook-Pro.local>
2018-07-23 15:16:57 -0400

    button

--- back_end/hairschool/admin.py
+++ back_end/hairschool/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from .models import hairschool
+from .models import SignUp
 
-admin.site.register(hairschool)
+admin.site.register(SignUp)
--- back_end/hairschool/urls.py
+++ back_end/hairschool/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path, include
+from . import views
+from rest_framework import routers
+
+
+router = routers.DefaultRouter()
+router.register('SignUp', views.SignUpView)
 
 urlpatterns = [
-    
+    path('',include(router.urls))
 ]
