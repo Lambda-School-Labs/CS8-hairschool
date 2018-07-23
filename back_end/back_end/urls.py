4462a2edce04cc749b1780eba0cd566154e50ae6
nourmhassan <nour-pop1@hotmail.com>
2018-07-23 01:00:28 -0700

    still trying in signIn help wanted lol

--- back_end/back_end/urls.py
+++ back_end/back_end/urls.py
@@ -14,9 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path ,include
+from rest_framework import routers
+from hairschool.api import SignUpViewset, SignInViewset
+
+router = routers.DefaultRouter()
+router.register(r'SignUp', SignUpViewset)
+router.register(r'SignIn', SignInViewset)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('hairschool.urls'))
+    path('', include(router.urls)),
 ]
--- back_end/hairschool/views.py
+++ back_end/hairschool/views.py
@@ -1,10 +1,1 @@
 from django.shortcuts import render
-from rest_framework import viewsets
-
-from .models import SignUp
-from .serializers import SignUpSerializer
-
-
-class SignUpView(viewsets.ModelViewSet):
-    queryset = SignUp.objects.all()
-    serializer_class = SignUpSerializer
