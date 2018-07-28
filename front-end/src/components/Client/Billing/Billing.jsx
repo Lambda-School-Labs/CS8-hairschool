3bfdfb6b1e642b486ad50dce8b6269983e565859
SaaSha <saasha.pina@gmail.com>
2018-07-28 03:19:29 -0400

    styling

--- front-end/src/components/Client/Billing/Billing.jsx
+++ front-end/src/components/Client/Billing/Billing.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import './Billing.css';
 import ClientNav from '../ClientNav/ClientNav';
+import { Link } from 'react-router-dom';
 import { Row, Col,} from 'reactstrap';
 import {Button, Panel, PanelHeader} from 'react-bootstrap';
 import Billing2 from './Billingform';
@@ -10,11 +11,18 @@ class Billing extends Component {
     render () {
         return (
             <div className="Billing">
-                <Row>
+
+                <nav className="App-header">
+                        <Link className="link signup" to="/SignUp" style={{textDecoration: 'none'}}> Sign Up </Link>
+                        <Link className="link signin" to="/SignIn" style={{textDecoration: 'none'}}> Sign In </Link>
+                        <Link className="link DAsignin" to="/Admin/Schedule" style={{textDecoration: 'none'}}> Demo Admin </Link>
+                </nav> 
+                <Row className="Container">
                     <Col sm="3">
                         <ClientNav/>
                     </Col>
                     <Col sm="9">
+                        <div className="Title"> Billing </div>
                         <div className="PaymentInfo">
                         <Panel>
                             <Panel.Heading>
@@ -27,8 +35,7 @@ class Billing extends Component {
                         </div>
                         <button className="BuyNowButton">Buy Now</button>
                     </Col>
-                </Row>
-               
+                </Row>               
             </div>
         )
     }
