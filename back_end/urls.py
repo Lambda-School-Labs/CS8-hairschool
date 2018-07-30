a88001ef58fe51382d16ba44d64d9a1bf59ff23c
nourmhassan <nour-pop1@hotmail.com>
2018-07-30 15:57:49 -0700

    some changes in url

--- back_end/urls.py
+++ back_end/urls.py
@@ -19,5 +19,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hairschool/', include('hairschool.urls')),
+    path('', include('hairschool.urls')),
 ]
--- hairschool/urls.py
+++ hairschool/urls.py
@@ -1,10 +1,12 @@
 from django.urls import include, path
 
 from . import views
+from rest_framework import routers
+
+
 
 urlpatterns = [
     path('', views.UserListView.as_view()),
     path('', include('rest_auth.urls')),
     path('registration/', include('rest_auth.registration.urls')),
-
 ]
