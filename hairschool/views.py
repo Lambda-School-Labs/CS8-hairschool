fcea5221dd6c023bb7bbb0f0ebb03ab79a1abee2
JohnJohnX4 <JohnJohnX4@gmail.com>
2018-08-02 19:51:38 -0400

    minor tweaks with views

--- hairschool/views.py
+++ hairschool/views.py
@@ -3,20 +3,22 @@ from rest_framework import generics
 from . import models
 from . import serializers
 
-
+# User Views
 class UserListView(generics.ListCreateAPIView):
     queryset = models.CustomUser.objects.all()
     serializer_class = serializers.UserSerializer
 
-
+# Appointment Views
 class AppointmentListView(generics.ListCreateAPIView):
     queryset = models.Appointment.objects.all()
     serializer_class = serializers.AppointmentSerializer
 
+# Services Views
 class ServiceListView(generics.ListCreateAPIView):
     queryset = models.Service.objects.all()
     serializer_class = serializers.ServiceSerializer
 
+# Stylist Views
 class StylistListView(generics.ListCreateAPIView):
     queryset = models.Stylist.objects.all()
     serializer_class = serializers.StylistSerializer
