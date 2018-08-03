5378c808e9047187f3b9e2f62085d518b9ed6b17
JohnJohnX4 <JohnJohnX4@gmail.com>
2018-08-03 17:30:38 -0400

    minor changes

--- src/components/Client/SignIn/SignIn.jsx
+++ src/components/Client/SignIn/SignIn.jsx
@@ -8,8 +8,14 @@ class SignIn extends React.Component {
 	// constructor to hold data as user types it
 	constructor(props) {
 		super(props);
-        this.login = { username: "", password: "" };
-        this.URL = "http://127.0.0.1:8000"; // change this to the heroku link later on
+		this.login = {
+			username: "",
+			password: ""
+		};
+		this.URL = "https://john-cs8-hairschool.herokuapp.com"; // change this to the heroku link later on
+		this.state = {
+			showError: false
+		}
 	}
 
 	// method to update this.login with text as user types it into Input fields
@@ -20,21 +26,27 @@ class SignIn extends React.Component {
 		this.login[name] = value;
 	};
 
-    // methodto handle what happens when  the user clicks on the submit button 
+	showError() {
+		return <div>Error logging in</div>;
+	}
+
+	// methodto handle what happens when  the user clicks on the submit button
 	buttonHandler(login, history) {
-        let {username, password} = login;
-        axios
-            // an axios call to the endpoint to login on the server, passing in the email and pasword
+		let { username, password } = login;
+		if (username === "") this.setState({ showError: true});
+		axios
+			// an axios call to the endpoint to login on the server, passing in the email and pasword
 			.post(`${this.URL}/rest-auth/login/`, { username, password })
 			.then(res => {
-                // take the token recieved and set it in localstorage to retrieve later
-                localStorage.setItem("auth_token", res.data.key);
-                // push the next page to the user after everything is done
+				// take the token recieved and set it in localstorage to retrieve later
+				localStorage.setItem("auth_token", res.data.key);
+				// push the next page to the user after everything is done
 				history.push("/User/Schedule");
 			})
 			.catch(err => {
-                // code out the error handling later on
+				// code out the error handling later on
 				console.log(err);
+				// this.setState({showError = true})
 			});
 	}
 
@@ -44,10 +56,13 @@ class SignIn extends React.Component {
 				<div className="SignInTitle">Sign In</div>
 
 				<Form className="SignInForm">
-
 					<FormGroup>
 						<Label for="Email">Username: </Label>
-						<Input type="text" name="username" onChange={this.handleInputChange} />
+						<Input
+							type="text"
+							name="username"
+							onChange={this.handleInputChange}
+						/>
 					</FormGroup>
 
 					<FormGroup>
@@ -58,11 +73,14 @@ class SignIn extends React.Component {
 							onChange={this.handleInputChange}
 						/>
 					</FormGroup>
+						{this.state.showError ? this.showError() : false}
 
-					<Button onClick={() => this.buttonHandler(this.login, this.props.history)} type="button">
+					<Button
+						onClick={() => this.buttonHandler(this.login, this.props.history)}
+						type="button"
+					>
 						Sign In
 					</Button>
-
 					<div>
 						No account?<Link
 							className="link account"
