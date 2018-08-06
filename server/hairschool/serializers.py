6414d0210e2c25994071de52fdfdc01d697ba7f5
JohnJohnX4 <JohnJohnX4@gmail.com>
2018-08-06 19:25:26 -0400

    fixed models, serializers, urls, views missing code

--- server/hairschool/serializers.py
+++ server/hairschool/serializers.py
@@ -6,3 +6,29 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.CustomUser
         fields = ('email', 'username', )
+
+
+class AppointmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Appointment
+        fields = ('date', 'time', 'Stylist', 'Service', )
+
+
+class ServiceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Service
+        fields = ('styling', 'price', )
+
+
+class StylistSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Stylist
+        fields = ('full_name', 'availability', )
+
+
+class User_FeedbackSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.User_Feedback
+        fields = ('Stylist', 'Service', 'Date', 'Time',
+                  'Consultation', 'Notes', 'On_time', 'Notes', 'Styling', 'Notes',
+                  'Customer_Service', 'Notes', 'Overall', 'Notes',)
