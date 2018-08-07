91408d21a31184506c0a86eda993044285aae49c
nourmhassan <nour-pop1@hotmail.com>
2018-08-06 17:25:08 -0700

    move some files

--- server/hairschool/forms.py
+++ server/hairschool/forms.py
@@ -18,24 +18,24 @@ class CustomUserChangeForm(UserChangeForm):
 
 
 
-class CreateAppointment(forms.ModelForm):
-    class Meta:
-        model = Appointment
-        fields = ( 'time', 'Stylist', 'Service', )
+# class CreateAppointment(forms.ModelForm):
+#     class Meta:
+#         model = Appointment
+#         fields = ( 'time', 'Stylist', 'Service', )
 
-class CreateService(forms.ModelForm):
-    class Meta:
-        model = Service
-        fields = ( 'styling', 'price', )
+# class CreateService(forms.ModelForm):
+#     class Meta:
+#         model = Service
+#         fields = ( 'styling', 'price', )
 
-class CreateStylist(forms.ModelForm):
-    class Meta:
-        model = Stylist
-        fields = ( 'full_name', 'availability', )        
+# class CreateStylist(forms.ModelForm):
+#     class Meta:
+#         model = Stylist
+#         fields = ( 'full_name', 'availability', )        
 
-class CreateUser_Feedback(forms.ModelForm):
-    class Meta:
-        model = User_Feedback
-        fields = ( 'Stylist', 'Service','Time',
-                   'Consultation','Notes','On_time','Notes','Styling','Notes',
-                   'Customer_Service','Notes','Overall','Notes',)
+# class CreateUser_Feedback(forms.ModelForm):
+#     class Meta:
+#         model = User_Feedback
+#         fields = ( 'Stylist', 'Service','Time',
+#                    'Consultation','Notes','On_time','Notes','Styling','Notes',
+#                    'Customer_Service','Notes','Overall','Notes',)
